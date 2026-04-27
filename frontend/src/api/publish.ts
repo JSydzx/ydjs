784021c361cd2335e0f46aacf1827abeb21f2752
hellomoom <158555999+hellomoom@users.noreply.github.com>
2026-04-27 14:40:34 +0800

    Delete frontend/src directory

--- frontend/src/api/publish.ts
+++
@@ -1,9 +0,0 @@
-import { http } from './http'
-
-export const createPost = (payload: {
-  title: string
-  content: string
-  topic?: string
-}): Promise<any> => {
-  return http.post('/posts', payload).then((res) => res)
-}
