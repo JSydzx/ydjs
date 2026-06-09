3863baaf1901e4579cba83ae4fe65c65bccf8c78
MeraDicycle <1376541252@qq.com>
2026-06-09 23:08:21 +0800

    修改docker配置

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    open: true,
+    open: false,
     proxy: {
       '/api': {
         target: 'http://backend:8080',
